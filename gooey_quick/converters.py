ecd603b30bf4d22c2ad12359c58cbb6a5da52ec4
jaca <vitouejj@gmail.com>
2023-09-02 19:05:04 +0200

    refactor gooey_quick.convert_to_argument

--- gooey_quick/converters.py
+++ gooey_quick/converters.py
@@ -8,6 +8,110 @@ from gooey_quick.introspection import Parameter
 from gooey_quick.types import DirectoryPath, SaveToPath, FileWithExtension
 
 
+def convert_optional(parameter: Parameter) -> dict[str, Any]:
+    return {
+        **DEFAULT_TYPE_INTERPRETATION[parameter.type_annotation],
+        'required': False,
+    }
+
+
+def convert_file_with_extension(parameter: Parameter) -> dict[str, Any]:
+    allowed_file_types = '|'.join((f'{filetype.upper()} (*.{filetype})|*.{filetype}' for filetype in parameter.args))
+    allowed_file_types += '|All files (*.*)|*.*'
+    return {
+        'type': FileWithExtension,
+        'widget': 'FileChooser',
+        'gooey_options': {
+            'wildcard': allowed_file_types,
+        },
+    }
+
+
+def convert_list(parameter: Parameter) -> dict[str, Any]:
+    if parameter.type_annotation is Path:
+        return {
+            'type': Path,
+            'nargs': '+',
+            'widget': 'MultiFileChooser',
+        }
+    elif parameter.origin is FileWithExtension:
+        return {
+            **convert_file_with_extension(
+                Parameter(
+                    parameter.name,
+                    parameter.args,
+                    parameter.docstring,
+                    parameter.default,
+                )
+            ),
+            'widget': 'MultiFileChooser',
+            'nargs': '+',
+        }
+    else:
+        raise ValueError(f'list of {parameter.type_annotation} cannot be translated into a Gooey widget!')
+
+
+def convert_enum(parameter: Parameter) -> dict[str, Any]:
+    return {
+        'type': parameter.type_annotation.__getitem__,
+        'choices': list(parameter.type_annotation),
+    }
+
+
+DEFAULT_ORIGIN_CONVERTERS = {
+    list:               convert_list,
+    Optional:           convert_optional,
+    FileWithExtension:  convert_file_with_extension
+}
+
+
+DEFAULT_TYPE_INTERPRETATION = {
+    str: {
+        'type': str,
+    },
+    int: {
+        'type': int,
+        'widget': 'IntegerField',
+    },
+    bool: {
+        'action': 'store_true',
+        'required': False,
+    },
+    float: {
+        'type': float,
+        'widget': 'DecimalField',
+    },
+    date: {
+        'type': date.fromisoformat,
+        'widget': 'DateChooser',
+    },
+    time: {
+        'type': time.fromisoformat,
+        'widget': 'TimeChooser',
+    },
+    Path: {
+        'type': Path,
+        'widget': 'FileChooser',
+        'gooey_options': {
+            'wildcard': "All files (*.*)|*.*",
+        },
+    },
+    SaveToPath: {
+        'type': SaveToPath,
+        'widget': 'FileSaver',
+    },
+    DirectoryPath: {
+        'type': DirectoryPath,
+        'widget': 'DirChooser',
+    },
+}
+
+
+DEFAULT_SUBTYPES_CONVERTERS = {
+    Enum: convert_enum,
+}
+
+
 def convert_to_argument(parameter: Parameter | Optional[Any]):
     """
     converts a parameter into dict of args for gooey.GooeyParser.add_argument
@@ -16,131 +120,47 @@ def convert_to_argument(parameter: Parameter | Optional[Any]):
     :raises ValueError: if parameter cannot be translated into a gooey widget
     :returns: dict of args to be passed int gooey.GooeyParser.add_argument
     """
-    if parameter.is_optional and parameter.type_annotation is not bool:
-        args = convert_to_argument(
-                Parameter(
-                    parameter.name,
-                    type_annotation=parameter.type_annotation.__args__[0],
-                    docstring=parameter.docstring,
-                    default=parameter.default,
-                )
-        )
-        args['required'] = False
-        return args
-    elif parameter.is_list and parameter.type_annotation.__args__[0] is Path:
-        args = dict(
-            action='store',
-            type=Path,
-            help=parameter.docstring,
-            nargs='+',
-            widget='MultiFileChooser',
-        )
-    elif parameter.is_list and parameter.type_annotation.__args__[0].__origin__ is FileWithExtension:
-        args = convert_to_argument(
-            Parameter(
-                parameter.name,
-                parameter.type_annotation.__args__[0],
-                docstring=parameter.docstring,
-                default=parameter.default,
-            )
-        )
-        args['widget'] = 'MultiFileChooser'
-        args['nargs'] = '+'
-    elif parameter.type_annotation is DirectoryPath:
-        args = dict(
-            action='store',
-            type=DirectoryPath,
-            help=parameter.docstring,
-            widget='DirChooser',
-        )
-    elif parameter.type_annotation is SaveToPath:
-        args = dict(
-            action='store',
-            type=SaveToPath,
-            help=parameter.docstring,
-            widget='FileSaver',
-        )
-    elif parameter.type_annotation is Path:
-        args = dict(
-            type=Path,
-            action='store',
-            help=parameter.docstring,
-            widget='FileChooser',
-            gooey_options={
-                'wildcard': "All files (*.*)|*.*",
-            },
-        )
-    elif hasattr(parameter.type_annotation, '__origin__') and parameter.type_annotation.__origin__ is FileWithExtension:
-        allowed_file_types = '|'.join((f'{filetype.upper()} (*.{filetype})|*.{filetype}' for filetype in parameter.type_annotation.__args__[0].__args__))
-        allowed_file_types += '|All files (*.*)|*.*'
-        args = dict(
-            action='store',
-            type=FileWithExtension,
-            widget='FileChooser',
-            help=parameter.docstring,
-            gooey_options={
-                'wildcard': allowed_file_types,
-            },
-        )
-    elif issubclass(parameter.type_annotation, Enum):
-        args = dict(
-            type=parameter.type_annotation.__getitem__,
-            action='store',
-            help=parameter.docstring,
-            choices=list(parameter.type_annotation),
-        )
-    elif parameter.type_annotation is int:
-        args = dict(
-            type=int,
-            action='store',
-            help=parameter.docstring,
-            widget='IntegerField',
-        )
-    elif parameter.type_annotation is float:
-        args = dict(
-            type=float,
-            action='store',
-            help=parameter.docstring,
-            widget='DecimalField',
-        )
-    elif parameter.type_annotation is bool and parameter.has_default_value:
-        args = dict(
-            action='store_true',
-            help=parameter.docstring,
-            gooey_options={
-                'initial_value': parameter.default,
-            }
-        )
-    elif parameter.type_annotation is date:
-        args = dict(
-            action='store',
-            help=parameter.docstring,
-            type=date.fromisoformat,
-            widget='DateChooser',
-        )
-    elif parameter.type_annotation is time:
-        args = dict(
-            action='store',
-            help=parameter.docstring,
-            type=time.fromisoformat,
-            widget='TimeChooser',
-        )
-    elif parameter.type_annotation is str:
-        args = dict(
-            action='store',
-            help=parameter.docstring,
-            type=str,
-        )
-    else:
-        raise ValueError(f'{parameter.type_annotation} cannot be translated into a Gooey widget!')
 
-    args['dest'] = parameter.name
-    args['metavar'] = parameter.name.capitalize().replace('_', ' ')
-    args['required'] = not parameter.type_annotation is bool
+    args = {
+        'dest': parameter.name,
+        'metavar': parameter.name.capitalize().replace('_', ' '),
+        'required': True,
+        'action': 'store',
+        'help': parameter.docstring,
+    }
+
+    if parameter.type_annotation is bool:
+        args['gooey_options'] = {
+            'initial_value': parameter.default,
+        }
 
     if parameter.has_default_value and parameter.type_annotation is not bool:
         args['default'] = parameter.default
 
+    try:
+        if parameter.origin in DEFAULT_ORIGIN_CONVERTERS:
+            convert = DEFAULT_ORIGIN_CONVERTERS[parameter.origin]
+            type_specific_args = convert(
+                Parameter(
+                    parameter.name,
+                    parameter.args,
+                    docstring=parameter.docstring,
+                    default=parameter.default,
+                )
+            )
+        elif parameter.type_annotation in DEFAULT_TYPE_INTERPRETATION:
+            type_specific_args = DEFAULT_TYPE_INTERPRETATION[parameter.type_annotation]
+        else:
+            for parent_type in DEFAULT_SUBTYPES_CONVERTERS:
+                if issubclass(parameter.type_annotation, parent_type):
+                    convert = DEFAULT_SUBTYPES_CONVERTERS[parent_type]
+                    type_specific_args = convert(parameter)
+                    break
+            else:
+                raise KeyError()
+    except KeyError:
+        raise ValueError(f'{parameter.type_annotation} cannot be translated into a Gooey widget!')
 
-    return args
+
+    return {**args, **type_specific_args}
 
